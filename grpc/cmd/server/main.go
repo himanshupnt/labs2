6ebc0f2921d70fb265093b9fd2941a9f44993f95
gopherland <fernand@gopherland.io>
2020-05-19 18:03:41 -0600

    updates

--- grpc/cmd/server/main.go
+++ grpc/cmd/server/main.go
@@ -1,4 +1,7 @@
-// Package main implements a server for Greeter service.
+// Copyright 2020 Imhotep Software
+// All material is licensed under the Apache License Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package main
 
 import (
--- grpc/internal/server/grepper_test.go
+++ grpc/internal/server/grepper_test.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Imhotep Software
+// All material is licensed under the Apache License Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package server_test
 
 import (
