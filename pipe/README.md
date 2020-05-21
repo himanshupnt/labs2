f328973243382e1f9864397dffe6fd99fd976632
gopherland <fernand@gopherland.io>
2020-05-21 14:07:35 -0600

    updates

---
+++ pipe/README.md
@@ -0,0 +1,23 @@
+<img src="../assets/gophernand.png" align="right" width="128" height="auto"/>
+
+<br/>
+<br/>
+<br/>
+
+# Pipeline
+
+---
+## <img src="../assets/lab.png" width="auto" height="32"/> Your Mission
+
+> Take a serial process and run it as a pipeline
+
+* Clone the [Labs Repo](https://github.com/gopherland/labs2)
+* Cd pipe
+* The given serial program computes MD5 digests from a collection of books.
+* Break out this serial process into a single stage concurrent pipeline.
+* BONUS! Use a multi-stage pipeline
+* BONUS! Benchmark your results
+
+---
+<img src="../assets/imhotep_logo.png" width="32" height="auto"/> © 2020 Imhotep Software LLC.
+All materials licensed under [Apache v2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ pipe/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"crypto/md5"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"sync"
+	"time"
+)
+
+type digest struct {
+	file string
+	sum  [md5.Size]byte
+	err  error
+}
+
+func (d *digest) digest(raw []byte) {
+	d.sum = md5.Sum(raw)
+}
+
+func main() {
+	var parallel bool
+	flag.BoolVar(&parallel, "p", false, "Toggle the run mode")
+	flag.Parse()
+
+	if parallel {
+		if err := md5Pipe("assets"); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	if err := md5Serial("assets"); err != nil {
+		panic(err)
+	}
+}
+
+func md5Serial(dir string) error {
+	defer func(t time.Time) {
+		fmt.Printf("Serial %v\n", time.Since(t))
+	}(time.Now())
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	mm := make(map[string][md5.Size]byte)
+	for _, f := range files {
+		raw, err := ioutil.ReadFile(filepath.Join(dir, f.Name()))
+		if err != nil {
+			return err
+		}
+		d := digest{file: f.Name()}
+		d.digest(raw)
+		mm[d.file] = d.sum
+	}
+
+	keys := make([]string, 0, len(mm))
+	for k := range mm {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("%x  %s\n", mm[k], k)
+	}
+
+	return nil
+}
+
+func md5Pipe(dir string) error {
+	<<!!YOUR_CODE!!>> -- implement a single stage pipeline to process digests
+}
