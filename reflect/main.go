2782d59a692a3390aa2d238a63def90938d7c0ea
gopherland <fernand@gopherland.io>
2020-08-31 13:00:15 -0600

    updates

--- reflect/main.go
+++ reflect/main.go
@@ -28,30 +28,22 @@ func wc(line string) int {
 	return len(tokens)
 }
 
-func readLines(path string) (chan string, chan error) {
-	out, errC := make(chan string), make(chan error, 1)
+func readLines(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-	go func(path string, out chan<- string, errC chan<- error) {
-		defer func() {
-			close(out)
-			close(errC)
-		}()
-		f, err := os.Open(path)
-		if err != nil {
-			errC <- err
-			return
-		}
+	var lines []string
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		return nil, err
+	}
 
-		sc := bufio.NewScanner(f)
-		for sc.Scan() {
-			out <- sc.Text()
-		}
-		if err := sc.Err(); err != nil {
-			errC <- err
-		}
-	}(path, out, errC)
-
-	return out, errC
+	return lines, nil
 }
 
 func hydrate(b interface{}) error {
@@ -63,16 +55,17 @@ func hydrate(b interface{}) error {
 	}
 
 	path := book.String()
-	out, errC := readLines(path)
-	var words, lines int
-	for line := range out {
-		words += wc(line)
-		lines++
-	}
-	if err, ok := <-errC; ok && err != nil {
+	lines, err := readLines(path)
+	if err != nil {
 		return err
 	}
-	rb.FieldByName("Words").SetInt(int64(words))
+	var wordCount, lineCount int
+	for _, line := range lines {
+		wordCount += wc(line)
+		lineCount++
+	}
+	rb.FieldByName("Words").SetInt(int64(wordCount))
+	rb.FieldByName("Lines").SetInt(int64(lineCount))
 
 	ibnT, _ := reflect.Indirect(rb).Type().FieldByName("IBN")
 	if _, ok := ibnT.Tag.Lookup("sha1"); ok {
@@ -80,7 +73,6 @@ func hydrate(b interface{}) error {
 	} else if _, ok := ibnT.Tag.Lookup("md5"); ok {
 		rb.FieldByName("IBN").SetString(fmt.Sprintf("%x", md5.Sum([]byte(path))))
 	}
-	rb.FieldByName("Lines").SetInt(int64(lines))
 
 	return nil
 }
@@ -93,5 +85,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%v\n", b)
+	fmt.Printf("%+v\n", b)
 }
