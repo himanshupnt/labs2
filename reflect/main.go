084d79516b2367fe43c72db9c26207b1215d644f
gopherland <fernand@gopherland.io>
2020-08-31 12:50:01 -0600

    updates

--- reflect/main.go
+++ reflect/main.go
@@ -29,6 +29,33 @@ func wc(line string) int {
 	return len(tokens)
 }
 
+// readLines read lines from a file on disk and returns a chan containing each lines read or an error.
+func readLines(path string) (chan string, chan error) {
+	out, errC := make(chan string), make(chan error, 1)
+
+	go func(path string, out chan<- string, errC chan<- error) {
+		defer func() {
+			close(out)
+			close(errC)
+		}()
+		f, err := os.Open(path)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		sc := bufio.NewScanner(f)
+		for sc.Scan() {
+			out <- sc.Text()
+		}
+		if err := sc.Err(); err != nil {
+			errC <- err
+		}
+	}(path, out, errC)
+
+	return out, errC
+}
+
 <<!!YOUR_CODE!!>> -- Define and implement the hydrate function
 
 func main() {
