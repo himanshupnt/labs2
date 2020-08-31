e8ff7c7f4f86f14398e9f8a09b65332eb6eee11e
gopherland <fernand@gopherland.io>
2020-08-31 13:07:24 -0600

    updates

--- reflect/main.go
+++ reflect/main.go
@@ -29,31 +29,22 @@ func wc(line string) int {
 	return len(tokens)
 }
 
-// readLines read lines from a file on disk and returns a chan containing each lines read or an error.
-func readLines(path string) (chan string, chan error) {
-	out, errC := make(chan string), make(chan error, 1)
+func readLines(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-	go func(path string, out chan<- string, errC chan<- error) {
-		defer func() {
-			close(out)
-			close(errC)
-		}()
-		f, err := os.Open(path)
-		if err != nil {
-			errC <- err
-			return
-		}
+	var lines []string
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		return nil, err
+	}
 
-		sc := bufio.NewScanner(f)
-		for sc.Scan() {
-			out <- sc.Text()
-		}
-		if err := sc.Err(); err != nil {
-			errC <- err
-		}
-	}(path, out, errC)
-
-	return out, errC
+	return lines, nil
 }
 
 <<!!YOUR_CODE!!>> -- Define and implement the hydrate function
